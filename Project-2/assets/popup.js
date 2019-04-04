dfd00a5f653c0b064e320ecc9167d9c65883bb57
odesonne <42941263+odesonne@users.noreply.github.com>
2019-04-03 23:31:51 -0400

    Add files via upload
    final push project 2

---
+++ Project-2/assets/popup.js
@@ -0,0 +1,138 @@
+// Button to open a modal
+$(document).ready(function (){
+    //ID or Class name of the button goes into the first section
+    $('#nebula-button').click(function() {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $("#nebula-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#nebula-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+
+//Rigel
+$(document).ready(function (){
+    $('#Rigel-button').click(function() {
+       
+        $("#Rigel-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#Rigel-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//Betelgeuse
+$(document).ready(function (){
+    $('#Betelgeuse-button').click(function() {
+       
+        $("#Betelgeuse-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#Betelgeuse-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//Castor
+$(document).ready(function (){
+    $('#Castor-button').click(function() {
+       
+        $("#Castor-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#Castor-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//Pollux
+$(document).ready(function (){
+    $('#Pollux-button').click(function() {
+       
+        $("#Pollux-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#Pollux-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//red-spot
+$(document).ready(function (){
+    $('#red-spot-button').click(function() {
+       
+        $("#red-spot-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#red-spot-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//rings
+$(document).ready(function (){
+    $('#rings-button').click(function() {
+       
+        $("#rings-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#rings-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//mare
+$(document).ready(function (){
+    $('#mare-button').click(function() {
+       
+        $("#mare-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#mare-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
+
+//Tycho
+$(document).ready(function (){
+    $('#Tycho-button').click(function() {
+       
+        $("#Tycho-modal").addClass("visible");
+        $("#grey-background").addClass("visible");
+        });
+});
+$(document).ready(function (){
+    $('#grey-background').click(function() {
+        $("#Tycho-modal").removeClass("visible");
+        $("#grey-background").removeClass("visible");
+        });
+});
